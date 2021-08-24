50c54eb8f608b0f91a5a37a1a715364f354399e4
PiotrIzw <58219259+PiotrIzw@users.noreply.github.com>
2021-08-24 23:40:49 +0200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  
+  const div = document.createElement('divvvv');
+  ReactDOM.render(<Apap />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
